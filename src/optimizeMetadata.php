43c4de0e62f763a7177a0945088f765e3db18a48
Andre L <andrelopezd@gmail.com>
2018-06-01 17:30:39 -0400

    Adds description meta

--- src/optimizeMetadata.php
+++ src/optimizeMetadata.php
@@ -8,26 +8,57 @@ require_once '../vendor/autoload.php';
 use Sunra\PhpSimple\HtmlDomParser;
 
 /**
- * Class
+ * Sprout Docs Scrapper Robot ಠ_ಠ
 */
 Class Optimize
 {
-
-    public function render()
+    public function run()
     {
-        $html = 'test.html';
-        $dom = HtmlDomParser::file_get_html($html);
+        $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('../docs/.vuepress/dist'));
+        foreach ($rii as $file) {
+
+            if ($file->isDir()){
+                continue;
+            }
+
+            $ext = pathinfo($file->getPathname(), PATHINFO_EXTENSION);
+            if ($ext == 'html'){
+                $this->processFile($file->getPathname());
+            }
+        }
+    }
+
+    private function processFile($htmlPath)
+    {
+        $dom = HtmlDomParser::file_get_html($htmlPath);
+        $defaultTitle = "Sprout Docs";
         $head = $dom->find('head', 0);
-        $elements = $dom->find('h1');
-        if (isset($elements[0])){
-            $title = $elements[0]->text();
-            $head->innertext .= "<meta name='description' content='{$title}'>";
+        $h1 = $dom->find('div[id="app"] h1');
+        $p = $dom->find('div[id="app"] p');
+
+        // Let's remove current title
+        $currentTitle = $dom->find('title', 0);
+        if ($currentTitle){
+            $currentTitle->innertext = '';
         }
 
-        $dom->save($html);
+        // Let's remove the current description
+        $currentDescription = $dom->find( "meta[name=description]", 0);
+        if ($currentDescription){
+            $currentDescription->innertext = '';
+        }
+
+        $title = isset($h1[0]) ? $h1[0]->text() : $defaultTitle;
+        $head->innertext = "<title> content='{$title}'</title>".$head->innertext;
+
+        $paragrah = isset($p[0]) ? $p[0]->text() : $title;
+
+        $head->innertext .= "<meta name='description' content='{$paragrah}'>";
+
+        $dom->save($htmlPath);
     }
 }
 
-$class = new Optimize();
+$optimize = new Optimize();
 
-$class->render();
+$optimize->run();
