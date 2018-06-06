7735d2735b77fc91b31b08c89f939f8fd4deea0c
Ben Parizek <ben@barrelstrengthdesign.com>
2018-06-06 11:11:06 -0400

    Adds improved command line support and meta image

--- src/optimizeMetadata.php
+++ src/optimizeMetadata.php
@@ -1,11 +1,15 @@
 <?php
+namespace barrelstrength\sproutdocs;
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once '../vendor/autoload.php';
+require_once realpath(__DIR__ . '/../vendor/autoload.php');
 
 use Sunra\PhpSimple\HtmlDomParser;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
 
 /**
  * Sprout Docs Scraper Robot ಠ_ಠ
@@ -14,7 +18,10 @@ Class Optimize
 {
     public function run()
     {
-        $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('../docs/.vuepress/dist'));
+
+        $docsDirectory = realpath(__DIR__ . '/../docs/.vuepress/dist');
+
+        $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($docsDirectory));
         $i = 0;
         foreach ($rii as $pos => $file) {
             if ($file->isDir()){
@@ -27,12 +34,13 @@ Class Optimize
                 $i++;
             }
         }
-        echo $i.' files optimized....';
+        echo $i.' files optimized.';
     }
 
     private function processFile($htmlPath)
     {
         $dom = HtmlDomParser::file_get_html($htmlPath);
+
         $defaultTitle = "Sprout Docs";
         $head = $dom->find('head', 0);
         $h1 = $dom->find('div[id="app"] div.content h1');
@@ -54,29 +62,32 @@ Class Optimize
         $title = isset($h1[0]) ? $h1[0]->text() : $defaultTitle;
         $head->innertext = "<title>'{$title}'</title>".$head->innertext;
 
-        $paragrah = isset($p[0]) ? $p[0]->text() : $title;
+        $paragraph = isset($p[0]) ? $p[0]->text() : $title;
 
-        $head->innertext .= "<meta name='description' content='{$paragrah}'>";
+        $head->innertext .= "<meta name='description' content='{$paragraph}'>";
+
+        $sproutLogoUrl = 'https://s3.amazonaws.com/sprout.barrelstrengthdesign.com-assets/content/plugins/sprout-plugins-logo.png';
 
         // Facebook
         $head->innertext .= "<meta property='og:type' content='website'>";
         $head->innertext .= "<meta property='og:site_name' content='Sprout Docs'>";
-        #$head->innertext .= "<meta property='og:url' content=''>";
+//        $head->innertext .= "<meta property='og:url' content=''>";
         $head->innertext .= "<meta property='og:title' content='{$title}'>";
-        $head->innertext .= "<meta property='og:description' content='{$paragrah}'>";
-        #$head->innertext .= "<meta property='og:image' content=''>";
-        #$head->innertext .= "<meta property='og:image:secure_url' content=''>";
-        #$head->innertext .= "<meta property='og:image:width' content='1280'>";
-        #$head->innertext .= "<meta property='og:image:type' content='image/svg+xml'>";
+        $head->innertext .= "<meta property='og:description' content='{$paragraph}'>";
+        $head->innertext .= "<meta property='og:image' content='{$sproutLogoUrl}'>";
+        $head->innertext .= "<meta property='og:image:secure_url' content='{$sproutLogoUrl}'>";
+        $head->innertext .= "<meta property='og:image:height' content='200'>";
+        $head->innertext .= "<meta property='og:image:width' content='200'>";
+        $head->innertext .= "<meta property='og:image:type' content='image/png'>";
         $head->innertext .= "<meta property='og:locale' content='en'>";
 
         // Twitter
         $head->innertext .= "<meta name='twitter:card' content='summary_large_image'>";
         $head->innertext .= "<meta name='twitter:site' content='@Barrel_Strength'>";
-        #$head->innertext .= "<meta name='twitter:url' content=''>";
+//        $head->innertext .= "<meta name='twitter:url' content=''>";
         $head->innertext .= "<meta name='twitter:title' content='{$title}'>";
-        $head->innertext .= "<meta name='twitter:description' content='{$paragrah}'>";
-        #$head->innertext .= "<meta name='twitter:image' content=''>";
+        $head->innertext .= "<meta name='twitter:description' content='{$paragraph}'>";
+        $head->innertext .= "<meta name='twitter:image' content='{$sproutLogoUrl}'>";
 
         $dom->save($htmlPath);
     }
