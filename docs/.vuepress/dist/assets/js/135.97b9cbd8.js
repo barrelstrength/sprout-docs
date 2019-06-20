5ddcf063680e711742a523295aa4a3a032fb82d8
Ben Parizek <ben@barrelstrengthdesign.com>
2019-06-19 23:06:49 -0400

    Updates build

--- docs/.vuepress/dist/assets/js/135.97b9cbd8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[135],{260:function(t,n,e){"use strict";e.r(n);var s=e(0),i=Object(s.a)({},function(){this.$createElement;this._self._c;return this._m(0)},[function(){var t=this.$createElement,n=this._self._c||t;return n("div",{staticClass:"content"},[n("h1",[this._v("Mailing Lists")])])}],!1,null,null,null);n.default=i.exports}}]);
