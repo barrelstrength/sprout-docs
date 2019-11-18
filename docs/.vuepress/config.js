29c5e275f055d232f7cc931088b70292529e2d06
Ben Parizek <ben@barrelstrengthdesign.com>
2019-11-18 10:14:47 -0500

    Adds spam protection to nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -63,6 +63,7 @@ module.exports = {
                         'reports',
                         'conditional-rules',
                         'integrations',
+                        'spam-protection',
                         'importing-data',
                         'plugin-settings'
                     ]
