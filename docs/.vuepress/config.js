e53bf8b7e072774ba7504639b0c3bb812e0f6d89
Ben Parizek <ben.parizek@gmail.com>
2023-02-19 19:38:25 -0500

    Updates config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -120,7 +120,6 @@ module.exports = {
                         'form-fields',
                         'entries',
                         'notifications',
-                        'data-studio',
                         'conditional-rules',
                         'integrations',
                         'spam-protection',
